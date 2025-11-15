a14863047ed8cbdfca5f3e256f8e9bad44a0c239
adela-b <adelaballiu1@gmail.com>
2025-11-15 20:49:27 +0100

    Add port to database connection string

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
-$dsn = "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8mb4";
+$dsn = "mysql:host=" . getenv("DB_HOST") . 
+       ";dbname=" . getenv("DB_NAME") . 
+       ";port=" . getenv("DB_PORT") . 
+       ";charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, getenv("DB_USER"), getenv("DB_PASS"), [
